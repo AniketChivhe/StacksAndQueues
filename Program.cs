f0d11479e35cc5ce230ef5a76aa4230fd9e77316
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-13 03:55:14 +0530

    [Aniket]UC3 and UC4 For Queue Added

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UC3_UC4_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UC3 And UC4 - Queue ");
+            Queue list = new Queue();
+            list.Enqueue(56);
+            list.Enqueue(30);
+            list.Enqueue(70);
+            list.Display();
+
+            for (int i = 0; i < 3; i++)
+            {
+                list.Dequeue();
+                list.Display();
+            }
+            list.Display();// this.top=this.top.next
+            Console.ReadKey();
+
+        }
+    }
+}
