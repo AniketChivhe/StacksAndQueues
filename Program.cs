dbf230e7ea67124c6fbf4ca12636baca12c97670
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-13 03:50:24 +0530

    [Aniket]UC1 Creating Stack

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UC_1 Adding Numbers in stacks");
+            Stack list = new Stack();
+            list.Push(70);
+            list.Push(30);
+            list.Push(56);
+            list.Display();
+            
+            Console.ReadKey();
+
+        }
+    }
+}
