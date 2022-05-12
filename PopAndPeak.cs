ccd15593578ba2c0d4f015676da5a7d8091e4db2
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-13 03:52:30 +0530

    [Aniket]UC2 pop and peak added

---
+++ PopAndPeak.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2_StackPopAndPeek
+{
+    internal class PopAndPeak
+    {
+        private Node top;
+        public PopAndPeak()
+        {
+            this.top = null;
+        }
+
+        //30
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " "); //56 30 70 
+                temp = temp.next;//1400
+            }
+        }
+
+        internal void Peak()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("\n{0} is in the top of the stack ", this.top.data);//NullrefrenceRxception
+        }
+
+        internal void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty, Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("\nValue popped is {0} ", this.top.data);
+            this.top = this.top.next;
+        }
+
+        internal void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peak();
+                Pop();
+            }
+            //this.top=null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UC2_StackPopAndPeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UC_2 Stack Pop And Peak");
+            PopAndPeak list = new PopAndPeak();
+            list.Push(70);
+            list.Push(30);
+            list.Push(56);
+            list.Display();
+
+            //Using For loop for delete all entry
+            for (int i = 0; i < 3; i++)
+            {
+                list.Peak();
+                list.Pop();
+                list.Display();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
